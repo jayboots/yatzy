fd7ab1456ca4e01847add7036142a14f8ddc2556
jayboots <84293704+jayboots@users.noreply.github.com>
2024-07-18 18:55:40 -0400

    feat(Assignment 03): :sparkles: implement complete scoring calculations in scorecard

--- public/app/models/ScoreCard.php
+++ public/app/models/ScoreCard.php
@@ -122,7 +122,7 @@ class ScoreCard {
                         $pts = array_sum($array);
                     }
                 }
-                // TODO: robustness to accept more than 2 dice... would be nice.
+                // TODO: Could add robustness to accept more than 2 dice... would be nice.
                 break;
             case "twoPairs":
                 if (strlen($stringDice) == 4){
@@ -134,102 +134,68 @@ class ScoreCard {
                     }
                 }
                 break;
-            // case "threeKind":
-            //     if (count($selectedHand) == 3){
-            //         $pattern = '/(\d)\1{2}/';
-            //         $outcomes = preg_match_all($pattern, $stringDice, $matches);
-            //         if ($outcomes) {
-            //             $pts = array_sum($selectedHand);
-            //             echo "This will give you " . $pts . " points!";
-            //         }
-            //         else{
-            //             echo $noMatchMsg;
-            //         }
-            //     }
-            //     else{
-            //         echo "You need to select three dice to score three of a kind.";
-            //     }
-            //     break;
-            // case "fourKind":
-            //     if (count($selectedHand) == 4){
-            //         $pattern = '/(\d)\1{3}/';
-            //         $outcomes = preg_match_all($pattern, $stringDice, $matches);
-            //         if ($outcomes) {
-            //             $pts = array_sum($selectedHand);
-            //             echo "This will give you " . $pts . " points!";
-            //         }
-            //         else{
-            //             echo $noMatchMsg;
-            //         }
-            //     }
-            //     else{
-            //         echo "You need to select four dice to score four of a kind.";
-            //     }
-            //     break;
-            // case "smallStraight":
-            //     // The combination 1-2-3-4-5. Score: 15 points (sum of all the dice)
-            //     $pattern = '/(12345)/';
-            //     $outcomes = preg_match_all($pattern, $stringDice, $matches);
-            //     if ($outcomes){
-            //         $pts=15;
-            //         echo $matches[0][0] . " gives you a small straight, and " . $pts . " points!";
-            //     }
-            //     else{
-            //         echo $noMatchMsg;
-            //     }
-            //     break;
-            // case "largeStraight":
-            //     // The combination 2-3-4-5-6. Score: 20 points (sum of all the dice).
-            //     $pattern = "/(23456)/";
-            //     $outcomes = preg_match_all($pattern, $stringDice, $matches);
-            //     if ($outcomes) {
-            //         $pts = 20;
-            //         echo $matches[0][0] . " gives you a large straight, and " . $pts . " points!";
-            //     } else {
-            //         echo $noMatchMsg;
-            //     }
-            //     // echo $outcomes;
-            //     break;
-            // case "fullHouse":
-            //     if (count($selectedHand) == 5) {
-            //         $pattern = "/(?<first>\d)(\k<first>){2}\d?(?<second>(?!\k<first>)\d)(\k<second>)|(?<third>\d)(\k<third>)\d?(?<fourth>(?!\k<third>)\d)(\k<fourth>){2}/";
-            //         $outcomes = preg_match_all($pattern, $stringDice, $matches);
-            //         if ($outcomes) {
-            //             $pts = array_sum($selectedHand);
-            //             echo "This will give you " . $pts . " points!";
-            //         } else {
-            //             echo $noMatchMsg;
-            //         }
-            //     } else {
-            //         echo "You need to select five dice to submit a full house.";
-            //     }
-            //     break;
-            // case "chance":
-            //     //Any combination of dice. Score: Sum of all the dice.
-            //     $pts = array_sum($selectedHand);
-            //     echo "This will give you " . $pts . " points";
-            //     break;
-            // case "yatzy":
-            //     if (count($selectedHand) == 5) {
-            //         $pattern = "/(\d)\1{4}/";
-            //         $outcomes = preg_match_all($pattern, $stringDice, $matches);
-            //         if ($outcomes) {
-            //             $pts = 50;
-            //             echo $matches[0][0] . " gives you a yatzy! And 50 pts! Congrats";
-            //         } else {
-            //             echo $noMatchMsg;
-            //         }
-            //     } else {
-            //         echo "You need to select five dice to submit a yatzy.";
-            //     }
-            //     break;
-            // default:
-            //     echo "Something went wrong. You selected: " . $scoreChoice;
-            //     // code block
-            //     break;
+            case "threeKind":
+                if (strlen($stringDice) == 3){
+                    $pattern = '/(\d)\1{2}/';
+                    $outcomes = preg_match_all($pattern, $stringDice, $matches);
+                    if ($outcomes) {
+                        $array = str_split($stringDice);
+                        $pts = array_sum($array);
+                    }
+                }
+                break;
+            case "fourKind":
+                if (strlen($stringDice) == 3){
+                    $pattern = '/(\d)\1{3}/';
+                    $outcomes = preg_match_all($pattern, $stringDice, $matches);
+                    if ($outcomes) {
+                        $array = str_split($stringDice);
+                        $pts = array_sum($array);
+                    }
+                }
+                break;
+            case "smallStraight":
+                // The combination 1-2-3-4-5. Score: 15 points (sum of all the dice)
+                $pattern = '/(12345)/';
+                $outcomes = preg_match_all($pattern, $stringDice, $matches);
+                if ($outcomes) {
+                    $array = str_split($stringDice);
+                    $pts = array_sum($array);
+                }
+                break;
+            case "largeStraight":
+                // The combination 2-3-4-5-6. Score: 20 points (sum of all the dice).
+                $pattern = "/(23456)/";
+                $outcomes = preg_match_all($pattern, $stringDice, $matches);
+                if ($outcomes) {
+                    $array = str_split($stringDice);
+                    $pts = array_sum($array);
+                }
+                break;
+            case "fullHouse":
+                if (strlen($stringDice) == 5){
+                    $pattern = "/(?<first>\d)(\k<first>){2}\d?(?<second>(?!\k<first>)\d)(\k<second>)|(?<third>\d)(\k<third>)\d?(?<fourth>(?!\k<third>)\d)(\k<fourth>){2}/";
+                    $outcomes = preg_match_all($pattern, $stringDice, $matches);
+                    if ($outcomes) {
+                        $array = str_split($stringDice);
+                        $pts = array_sum($array);
+                    }
+                }
+                break;
+            case "chance":
+                $array = str_split($stringDice);
+                $pts = array_sum($array);
+                break;
+            case "yatzy":
+                if (strlen($stringDice) == 5){
+                    $pattern = "/(\d)\1{4}/";
+                    $outcomes = preg_match_all($pattern, $stringDice, $matches);
+                    if ($outcomes) {
+                        $pts = 50;
+                    }
+                }
+                break;
         }
-
         return $pts;
     }
-
 }
